0061e903217d93ca55e541394e09b18d293b6bf9
DouCode <=>
2022-04-20 19:17:02 +0800

    docker-compose

---
+++ .history/api/handlers/post_20220420182031.go
@@ -0,0 +1,236 @@
+package handlers
+
+import (
+	"building-distributed-app-in-gin-chapter06/api/common"
+	"building-distributed-app-in-gin-chapter06/api/models"
+	"building-distributed-app-in-gin-chapter06/api/response"
+	"building-distributed-app-in-gin-chapter06/api/vo"
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type PostController struct {
+	collection  *mongo.Collection
+	ctx         context.Context
+	redisClient *redis.Client
+}
+
+func NewPostController(ctx context.Context, collection *mongo.Collection, redisClient *redis.Client) *PostController {
+	return &PostController{
+		collection:  collection,
+		ctx:         ctx,
+		redisClient: redisClient,
+	}
+}
+
+func (p PostController) Create(c *gin.Context) {
+	var requestPost vo.CreatePostRequest
+	if err := c.ShouldBindJSON(&requestPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var post = models.Post{}
+	post.Title = requestPost.Title
+	post.Content = requestPost.Content
+	post.CategoryId, _ = strconv.Atoi(requestPost.CategoryId)
+	post.HeadImg = requestPost.HeadImg
+	post.ID = primitive.NewObjectID()
+	post.PublishedAt = time.Now()
+	_, err := p.collection.InsertOne(p.ctx, post)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting a new post"})
+		return
+	}
+
+	log.Println("Remove data from Redis")
+	p.redisClient.Del("posts")
+
+	c.JSON(http.StatusOK, post)
+}
+
+func (p PostController) Update(c *gin.Context) {
+	id := c.Param("id")
+	var post models.Post
+	if err := c.ShouldBindJSON(&post); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := p.collection.UpdateOne(p.ctx, bson.M{
+		"_id": objectId,
+	}, bson.D{{"$set", bson.D{
+		{"title", post.Title},
+		{"content", post.Content},
+	}}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	response.Success(c, gin.H{"post": post}, "更新成功")
+}
+
+func (p PostController) Show(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	cur := p.collection.FindOne(p.ctx, bson.M{
+		"_id": objectId,
+	})
+	var post models.Post
+	err := cur.Decode(&post)
+	if err != nil {
+		response.Fail(c, nil, "文章不存在")
+		return
+	}
+
+	response.Success(c, gin.H{"data": post}, "成功")
+}
+
+func (p PostController) Edit(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	cur := p.collection.FindOne(p.ctx, bson.M{
+		"_id": objectId,
+	})
+	var post models.Post
+	err := cur.Decode(&post)
+	if err != nil {
+		response.Fail(c, nil, "文章不存在")
+		return
+	}
+
+	response.Success(c, gin.H{"data": post}, "成功")
+}
+
+func (p PostController) Delete(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := p.collection.DeleteOne(p.ctx, bson.M{
+		"_id": objectId,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	response.Success(c, gin.H{"post": ""}, "删除成功")
+}
+
+func (p PostController) NewBlog(c *gin.Context) {
+	var requestPost vo.CreateBlogRequest
+	if err := c.ShouldBindJSON(&requestPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	tokenString := c.GetHeader("Authorization")
+	_, claims, _ := common.ParseToken(tokenString)
+	userName := claims.UserName
+
+	var post = models.Post{}
+	post.Title = requestPost.Title
+	post.Content = requestPost.Content
+	post.Tags = requestPost.TagStr
+	post.ID = primitive.NewObjectID()
+	post.PublishedAt = time.Now()
+	post.UserName = userName
+
+	_, err := p.collection.InsertOne(p.ctx, post)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting a new post"})
+		return
+	}
+
+	log.Println("Remove data from Redis")
+	p.redisClient.Del("posts")
+
+	c.JSON(http.StatusOK, post)
+}
+
+func (p PostController) EditBlog(c *gin.Context) {
+	id := c.Param("id")
+	var requestPost vo.CreateBlogRequest
+	if err := c.ShouldBindJSON(&requestPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var post = models.Post{}
+	post.Title = requestPost.Title
+	post.Content = requestPost.Content
+	post.Tags = requestPost.TagStr
+	post.UpdatedAt = time.Now()
+
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	_, err := p.collection.UpdateOne(p.ctx, bson.M{
+		"_id": objectId,
+	}, bson.D{{"$set", bson.D{
+		{"title", post.Title},
+		{"content", post.Content},
+	}}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	response.Success(c, gin.H{"post": post}, "更新成功")
+}
+
+func (p PostController) PageList(c *gin.Context) {
+	// 获取分页参数
+	pageNum, _ := strconv.Atoi(c.DefaultQuery("currentPage", ""))
+	pageSize, _ := strconv.Atoi(c.DefaultQuery("pageSize", ""))
+
+	//val, err := p.redisClient.Get("posts").Result()
+	//if err == redis.Nil {
+	log.Printf("Request to MongoDB")
+
+	findOptions := options.Find()
+	// Sort by `price` field descending
+	findOptions.SetSort(bson.D{{"publishedAt", -1}})
+	cur, err := p.collection.Find(p.ctx, bson.M{}, findOptions)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(p.ctx)
+
+	posts := make([]models.Post, 0)
+	for cur.Next(p.ctx) {
+		var post models.Post
+		cur.Decode(&post)
+		posts = append(posts, post)
+	}
+	total := len(posts)
+
+	//data, _ := json.Marshal(posts)
+	//p.redisClient.Set("posts", string(data), 0)
+	a := (pageNum - 1) * pageSize
+	b := pageNum * pageSize
+	if b > total {
+		b = total
+	}
+	response.Success(c, gin.H{"rows": posts[a:b], "total": total}, "成功")
+	//} else if err != nil {
+	//	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	//	return
+	//} else {
+	//	log.Printf("Request to Redis")
+	//	posts := make([]models.Post, 0)
+	//	total := len(posts)
+	//	json.Unmarshal([]byte(val), &posts)
+	//	a := (pageNum - 1) * pageSize
+	//	b := pageNum * pageSize
+	//	response.Success(c, gin.H{"data": posts[a:b], "total": total}, "成功")
+	//}
+}
---
+++ .history/api/main_20220420184029.go
@@ -0,0 +1,98 @@
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/PacktPublishing/Building-Distributed-Applications-in-Gin.
+//
+//	Schemes: http
+//  Host: localhost:8080
+//	BasePath: /
+//	Version: 1.0.0
+//	Contact: Mohamed Labouardy <mohamed@labouardy.com> https://labouardy.com
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+// swagger:meta
+package main
+
+import (
+	"building-distributed-app-in-gin-chapter06/api/handlers"
+	"building-distributed-app-in-gin-chapter06/api/middleware"
+	"context"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var authHandler *handlers.AuthHandler
+var recipesHandler *handlers.RecipesHandler
+var postController *handlers.PostController
+
+func init() {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	collectionPost := client.Database(os.Getenv("MONGO_DATABASE")).Collection("posts")
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	status := redisClient.Ping()
+	log.Println(status)
+
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection, redisClient)
+
+	postController = handlers.NewPostController(ctx, collectionPost, redisClient)
+
+	collectionUsers := client.Database(os.Getenv("MONGO_DATABASE")).Collection("users")
+	authHandler = handlers.NewAuthHandler(ctx, collectionUsers)
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(middleware.CORSMiddleware())
+
+	router.POST("/user/register", authHandler.RegisterHandler)
+	router.POST("/user/login", authHandler.SignInHandler)
+	router.GET("/user/logout", authHandler.LogOutHandler)
+	router.GET("/site", authHandler.IntroductionHandler)
+
+	router.GET("/tag", authHandler.TagHandler)
+
+	router.GET("/api/auth/info", authHandler.AuthMiddleware(), authHandler.Info)
+	//router.POST("/refresh", authHandler.RefreshHandler)
+
+	router.GET("/recipes", recipesHandler.ListRecipesHandler)
+	authorized := router.Group("/")
+	authorized.Use(authHandler.AuthMiddleware())
+	{
+		authorized.POST("/recipes", recipesHandler.NewRecipeHandler)
+		authorized.PUT("/recipes/:id", recipesHandler.UpdateRecipeHandler)
+		authorized.DELETE("/recipes/:id", recipesHandler.DeleteRecipeHandler)
+		authorized.GET("/recipes/:id", recipesHandler.GetOneRecipeHandler)
+	}
+
+	postRoutes := router.Group("/")
+	postRoutes.GET("/blog/:id"+"/false", postController.Show)
+	postRoutes.GET("/blog/:id"+"/true", postController.Edit)
+	postRoutes.GET("/blog/home", postController.PageList)
+	postRoutes.Use(authHandler.AuthMiddleware())
+	postRoutes.POST("/blog", postController.NewBlog)
+	postRoutes.PUT("/blog/:id", postController.EditBlog)
+	postRoutes.DELETE("/blog/admin/:id", postController.Delete)
+
+	router.Run(":1016")
+}
